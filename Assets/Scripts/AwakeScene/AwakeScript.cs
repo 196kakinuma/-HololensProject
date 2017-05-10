584d49678debe7f59a27907e46f44194a985b9cf
196kakinuma <ikumu@IkumunoMacBook-Pro.local>
2017-05-10 14:30:36 +0900

    First commit

---
+++ Assets/Scripts/AwakeScene/AwakeScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace AwakeScene
+{
+    class AwakeScript : MonoBehaviour
+    {
+        /// <summary>
+        /// Hololensアプリケーションは起動時に軽いシーンを挟むと良い
+        /// </summary>
+        void Awake()
+        {
+            SceneManager.LoadScene("TestMainScene");
+        }
+    }
+}
